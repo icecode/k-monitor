9a4c4552b5aa4fc11accc52c837d33096235f728
icecoder <980668011@qq.com>
2021-04-13 09:33:22 +0800

    feat: 增加监听udp设置选项

--- src/syslog_server.rs
+++ src/syslog_server.rs
@@ -2,19 +2,31 @@ use tokio::net::UdpSocket;
 use std::collections;
 use std::str;
 use crate::log_parse;
+use lazy_static::lazy_static;
 
-use prometheus::{register_int_counter_vec, register_histogram_vec};
+use prometheus::{IntCounter, register_int_counter, register_int_counter_vec, register_histogram_vec};
 
-pub async fn listen(adds: &str) -> std::io::Result<()> {
-    let sock = UdpSocket::bind(adds).await?;
+lazy_static! {
+
+    static ref UDP_RECEIVE_COUNTER:IntCounter = register_int_counter!("syslog_udp_receive", "receive syslog udp log counter").unwrap();
+
+    static ref UDP_PARSE_COUNTER:IntCounter = register_int_counter!("syslog_udp_parse", "receive syslog udp parse success counter").unwrap();
+
+
+}
+
+pub async fn udp_listen(adds: String) -> std::io::Result<()> {
+    let sock = UdpSocket::bind(adds.clone()).await?;
     let mut buf = [0u8; 4096];
     let mut service_counter_map = collections::HashMap::new();
     let mut service_duration_map = collections::HashMap::new();
+    println!("syslog udp://{}", &adds);
     loop {
         let (len, _) = sock.recv_from(&mut buf).await?;
+        UDP_RECEIVE_COUNTER.inc();
         if let Ok(s) = str::from_utf8(&buf[..len]) {
             if let Some(log) = log_parse::try_parse_from_json(s) {
-                println!("rev:{:?}", &log);
+                UDP_PARSE_COUNTER.inc();
                 let req_counter_name = log.service.clone() + &"_request".to_string();
                 if !service_counter_map.contains_key(&req_counter_name) {
                     let service_counter = register_int_counter_vec!(req_counter_name.as_str(), "The HTTP request counter", &["domain", "backend"]).unwrap();
