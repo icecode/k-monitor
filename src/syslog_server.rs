2c86dc81944441e2ebdca4df149a1f79205365d7
icecoder <980668011@qq.com>
2021-04-19 17:35:16 +0800

    fix:秒数解析

--- src/syslog_server.rs
+++ src/syslog_server.rs
@@ -25,7 +25,12 @@ pub async fn udp_listen(adds: String) -> std::io::Result<()> {
     loop {
         let (len, _) = sock.recv_from(&mut buf).await?;
         NGINX_SYSLOG_UDP_RECEIVE_COUNTER.inc();
-        if let Ok(s) = str::from_utf8(&buf[21..len]) {
+        println!("syslog receive value:{}", str::from_utf8(&buf[..len]).unwrap());
+        if len < 41 {
+            println!("syslog receive illegal value:{}", str::from_utf8(&buf[..len]).unwrap());
+            continue;
+        }
+        if let Ok(s) = str::from_utf8(&buf[41..len]) {
             if let Some(log) = try_parse_from_json(s) {
                 NGINX_SYSLOG_UDP_PARSE_COUNTER.inc();
                 let req_counter_name = log.service.clone() + &"_request".to_string();
@@ -44,9 +49,8 @@ pub async fn udp_listen(adds: String) -> std::io::Result<()> {
                     service_duration_map.insert(req_duration_name.clone(), service_duration);
                 }
                 service_duration_map.get(&req_duration_name).map ( | service_duration | {
-                    let dur_sec = log.request_time as f64 / 1000_f64;
-                    service_duration.with_label_values(&["all", "all"]).observe(dur_sec);
-                    service_duration.with_label_values(&[log.domain.as_str(), "127.0.0.1"]).observe(dur_sec);
+                    service_duration.with_label_values(&["all", "all"]).observe(log.request_time);
+                    service_duration.with_label_values(&[log.domain.as_str(), "127.0.0.1"]).observe(log.request_time);
                 });
             }
         }
@@ -55,15 +59,13 @@ pub async fn udp_listen(adds: String) -> std::io::Result<()> {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AccessLog {
-    pub timestamp: i32,
     pub remote_addr: String,
     pub service: String,
     pub domain: String,
     pub request: String,
     pub status: u32,
     pub sent_body_bytes: u32,
-    pub upstream_response_time: u32,
-    pub request_time: u32
+    pub request_time: f64
 }
 
 pub fn try_parse_from_json(access_line: &str) -> Option<AccessLog> {
@@ -71,4 +73,19 @@ pub fn try_parse_from_json(access_line: &str) -> Option<AccessLog> {
         Ok(log) => Some(log),
         _ => None
     }
+}
+
+
+#[test]
+fn parse_log() {
+    let json_str = r#"{"timestamp": "19/Apr/2021:09:21:27 +0000", "remote_addr": "172.17.0.1", "service": "localhost", "domain": "127.0.0.1", "request": "GET /favicon.ico HTTP/1.1", "status": 500, "sent_body_bytes": 580, "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36", "x_forwarded": "-", "up_addr": "-","up_host": "-","upstream_response_time": "-","request_time": 0.000}"#;
+    let len = json_str.len();
+    println!("{}", json_str);
+    let log = try_parse_from_json(json_str).unwrap();
+    assert_eq!("172.17.0.1", log.remote_addr);
+    assert_eq!("localhost", log.service);
+    assert_eq!("127.0.0.1", log.domain);
+    assert_eq!(500, log.status);
+    assert_eq!(580, log.sent_body_bytes);
+    assert_eq!(0.000_f64, log.request_time);
 }
